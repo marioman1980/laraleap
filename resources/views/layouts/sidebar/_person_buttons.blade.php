119be3e7a485ce5bd01dc25d19f34c8596c23e08
James <james@jkamradcliffe.net>
2018-08-07 21:02:17 +0100

    Correct _person_buttons layout

--- resources/views/layouts/sidebar/_person_buttons.blade.php
+++ resources/views/layouts/sidebar/_person_buttons.blade.php
@@ -2,15 +2,15 @@
 	@if ($topic->staff)
 		@if ($controller_name == 'People')
 			<a href="#" class="btn btn-default"><i class="fa fa-fw fa-calendar"></i> Timetable</a>
-		@else
-			@if ($controller_name == 'Timetables')
-				<a href="/people/{{ $topic->mis_id }}" class="btn btn-default">
-					<i class="fa fa-fw fa-home"></i> Home</a>
-				</a>
-			@else
-				<a href="#" class="btn btn-default"><i class="fa fa-fw fa-calendar"></i> Timetable</a>	
-			@endif 	
 		@endif
+	@else
+		@if ($controller_name == 'Timetables')
+			<a href="/people/{{ $topic->mis_id }}" class="btn btn-default">
+				<i class="fa fa-fw fa-home"></i> Home</a>
+			</a>
+		@else
+			<a href="#" class="btn btn-default"><i class="fa fa-fw fa-calendar"></i> Timetable</a>	
+		@endif 	
 	@endif
 	@if ($user->staff)
 		<a href="#" class="btn btn-default slchk"><i class="fa fa-fw fa-bolt"></i> EBS (IE Only)</a>
