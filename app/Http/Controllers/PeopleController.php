47440e0597ac826a0abdf465b08d4dd4ac343109
James <james@jkamradcliffe.net>
2018-08-14 20:37:36 +0100

    Adding settings to json

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -29,8 +29,8 @@ class PeopleController extends BaseController
         $progresses = $this->get_progress_data($topic);
 
 
-        setting(['nums' => [5,7,9]])->save();
-        var_dump(setting('students_create_events'));
+        setting(['maintenance_mode' => 'test'])->save();
+        var_dump(setting('admin_users'));
 
 
         // foreach($progresses as $progress) {
